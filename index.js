de7069f5ff3ef5790f917aad16dcb33f79e3b2ac
TomAylen <68003895+TomAylen@users.noreply.github.com>
2020-08-25 09:26:07 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-app.get('/',(req, res)=>{
-  res.send('A Simple Web App .....')
-})
-
-app.listen(8080,()=>{
-  console.log('server runnig on port 8080')
-})
